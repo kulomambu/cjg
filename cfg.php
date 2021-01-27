ebf6fbebc7f8e8a6747a068e9392c2eb6bdb95c7
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-27 23:53:24 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,11 @@
+<?php
+
+$cfduid="d30fb80db473b60f8398ed5ee42ab3e391611764570";
+
+$_ga="GA1.1.47710372.1611764571";
+
+$phpsessid="d7k25l6rdpf7u9rrihirr4r0gm";
+
+$_ga_XD6NKYR2JE="GS1.1.1611764571.1.1.1611764991.0";
+
+$UserAgent="Mozilla/5.0 (Linux; Android 9; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
